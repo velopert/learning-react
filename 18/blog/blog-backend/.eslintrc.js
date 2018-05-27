910e97f10ebc30ea0044d5b3e1408fd6357c58f4
velopert <public.velopert@gmail.com>
2018-05-27 14:47:55 +0900

    fixed typo

--- 18/blog/blog-backend/.eslintrc.js
+++ 18/blog/blog-backend/.eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-      "no-unused-vars": 1,
-      "comma-dangle": 0,
-      "eol-last": 0,
-      "no-console": 0
-    }
-  
+  "extends": "airbnb-base",
+  "rules": {
+    "no-unused-vars": 1,
+    "comma-dangle": 0,
+    "eol-last": 0,
+    "no-console": 0
+  }
+}
