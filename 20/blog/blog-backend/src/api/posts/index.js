7db49b755cedec7360e29a721631f90e16618c3f
velopert <public.velopert@gmail.com>
2018-06-04 02:23:31 +0900

    added blog-backend to project 20

---
+++ 20/blog/blog-backend/src/api/posts/index.js
@@ -0,0 +1,12 @@
+const Router = require('koa-router');
+const postsCtrl = require('./posts.ctrl');
+
+const posts = new Router();
+
+posts.get('/', postsCtrl.list);
+posts.post('/', postsCtrl.write);
+posts.get('/:id', postsCtrl.checkObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.checkObjectId, postsCtrl.update);
+
+module.exports = posts;
