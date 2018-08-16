a3ada1e5f96aba0c3e069d732952f4dcd0a5d1ad
velopert <public.velopert@gmail.com>
2018-08-16 13:47:28 +0900

    use id to get index of the todo item

--- 14/todo-list/src/modules/todos.js
+++ 14/todo-list/src/modules/todos.js
@@ -37,11 +37,14 @@ export default handleActions({
     }));
   },
   [TOGGLE]: (state, action) => {
-    const { payload: index } = action;
-    // = const index = action.payload;
-    /* 비구조화 할당을 통하여 index라는 레퍼런스에 action.payload란 값을 넣습니다.
+    const { payload: id } = action;
+    // = const id = action.payload;
+    /* 비구조화 할당을 통하여 id라는 레퍼런스에 action.payload란 값을 넣습니다.
     이 작업이 필수는 아니지만, 나중에 이 코드를 보게 되었을 때 여기서의 payload가
     어떤 값을 의미하는지 이해하기 쉬워집니다. */
+
+    // 전달받은 id 를 가지고 index 를 조회합니다.
+    const index = state.findIndex(todo => todo.get('id') === id);
 
     // updateIn을 통해 현재 값을 참조하여 반대값으로 설정합니다.
     return state.updateIn([index, 'done'], done => !done);
@@ -51,7 +54,8 @@ export default handleActions({
     */
   },
   [REMOVE]: (state, action) => {
-    const { payload: index } = action;
+    const { payload: id } = action;
+    const index = state.findIndex(todo => todo.get('id') === id);
     return state.delete(index);
   }
 }, initialState);
