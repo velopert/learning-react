3d72bd8dd410a52842b893057710e68145a5ce6f
velopert <public.velopert@gmail.com>
2019-06-03 03:43:39 +0900

    Last version of chapter 13 App.js

--- 13/router-tutorial/src/App.js
+++ 13/router-tutorial/src/App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profiles from './Profiles';
+import HistorySample from './HistorySample';
 
 const App = () => {
   return (
     <div>
-      <Route path="/" component={Home} exact={true} />
-      <Route path={['/about', '/info']} component={About} />
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">History 예제</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path={['/about', '/info']} component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route
+          // path를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+      </Switch>
     </div>
   );
 };
