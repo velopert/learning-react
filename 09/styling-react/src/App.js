8e1f791d68b24ff7f0a1d4711bd265237f8b5f78
velopert <public.velopert@gmail.com>
2020-02-08 21:17:08 +0900

    Update styling-react

--- 09/styling-react/src/App.js
+++ 09/styling-react/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import StyledComponent from './StyledComponent';
+import SassComponent from './SassComponent';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <StyledComponent />
+        <SassComponent />
       </div>
     );
   }
