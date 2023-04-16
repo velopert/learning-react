2f5ab6c0c8f806ca11216a48300acc5c83e2be38
velopert <public.velopert@gmail.com>
2023-04-17 04:13:35 +0900

    Update chapter 13

--- 13/router-tutorial/src/index.js
+++ 13/router-tutorial/src/index.js
@@ -1,12 +1,16 @@
+
+import { BrowserRouter } from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById('root')
 );
+
+
