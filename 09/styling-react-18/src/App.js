b5aea6bbc3b24f27b8beb4f1300a518be9a101f7
velopert <public.velopert@gmail.com>
2023-04-17 04:02:24 +0900

    Update chapter 9

--- 09/styling-react-18/src/App.js
+++ 09/styling-react-18/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SassComponent from "./SassComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
 }
 
 export default App;
