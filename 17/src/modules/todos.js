0add6a2f6531a628150a5e601a9977feb561cae7
velopert <public.velopert@gmail.com>
2023-04-24 00:55:30 +0900

    fix: minor code style

--- 17/src/modules/todos.js
+++ 17/src/modules/todos.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import {produce} from 'immer';
+import { produce } from 'immer';
 
 const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경함
 const INSERT = 'todos/INSERT'; // 새로운 todo 를 등록함
